4a3e18014925e14d3bd1ccb403867953e9dfa5e3
JuHai Wang <39918621+SeaGod2019@users.noreply.github.com>
2019-05-08 23:18:08 +0800

    Update alu.v

--- 结构示例/datapath/alu.v
+++ 结构示例/datapath/alu.v
@@ -11,7 +11,7 @@ beq   101
 
 module alu(
     A, B, ALUctrl,
-    ALU, beq, carrier
+    ALU, zero, carrier
 );
 
 input   [31:0]          A;                  // the 1st input data
@@ -19,13 +19,13 @@ input   [31:0]          B;                  // the 2nd input data
 input   [2:0]           ALUctrl;               
 output  [31:0]          ALU;                  // calculation result 
 output                  carrier;   
-output                  beq;            
+output                  zero;            
 
 reg     [32:0]          tmp; 
 
 assign  ALU = tmp[31:0] ;
 assign  carrier = (tmp[32]==1) ? 1 : 0 ;
-assign  beq = (tmp[31:0]==0) ? 1 : 0 ;
+assign  zero = (tmp[31:0]==0) ? 1 : 0 ;
 
 always  @( * )
 	case (ALUctrl)
